265660bd7f89974608b00a4f4a2f2ce331cf12b3
fengchao0223.ts <fengchao0223@thundersoft.com>
2019-09-05 16:18:41 +0800

    chapter7

---
+++ otto/src/Chapter7/TestCollections.java
@@ -0,0 +1,22 @@
+package Chapter7;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class TestCollections {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//同步集合对象,这样获取的就是线程安全版本的List Set Map了
+		List list = Collections.synchronizedList(new ArrayList());
+		Set s = Collections.synchronizedSet(new HashSet());
+		Map m = Collections.synchronizedMap(new HashMap());
+		
+	}
+
+}
--- otto/src/Chapter7/TestMap.java
+++ otto/src/Chapter7/TestMap.java
@@ -1,10 +1,59 @@
 package Chapter7;
 
+import java.util.Hashtable;
+
+class ABB{
+	int count;
+
+	public ABB(int count) {
+		super();
+		this.count = count;
+	}
+	@Override
+	public int hashCode() {
+		// TODO ABButo-generated method stub
+		return count;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		// TODO ABButo-generated method stub
+		if(obj==this) {
+			return true;
+		}
+		if(obj!=null && obj.getClass()==ABB.class) {
+			ABB a = (ABB) obj;
+			if(this.count == a.count) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
+class BB{
+	@Override
+	public boolean equals(Object obj) {
+		// TODO ABButo-generated method stub
+		return true;
+	}
+}
 public class TestMap {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		// TODO ABButo-generated method stub
+		Hashtable ht = new Hashtable();
+		ht.put(new ABB(12306),"花果山");
+		ht.put(new ABB(110),"英雄联盟赛事");
+		ht.put(new ABB(1803),new BB());
+		System.out.println(ht);
+		//因为B不管怎样都会返回true,所以任意字符串都会返回true
+		System.out.println(ht.containsValue("测试"));
+		System.out.println(ht.containsKey(new ABB(12306)));
+		ht.remove(new ABB(110));
+		for (Object key : ht.keySet()) {
+			System.out.println(key+"   ");
+			System.out.println(ht.get(key)+"\n");
+		}
+		System.out.println(ht.size());
 	}
 
 }
