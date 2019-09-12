562812c215552eef9d48ad4f596a8df1f4f1d340
fengchao0223.ts <fengchao0223@thundersoft.com>
2019-09-12 11:05:15 +0800

    chapter13

---
+++ otto/src/chapter13/jdbcTest.java
@@ -0,0 +1,19 @@
+package chapter13;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class jdbcTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//数据库连接
+		Connection connection = null;
+		//预编译Statement
+		PreparedStatement ps = null;
+		//结果集
+		ResultSet rs = null;
+	}
+
+}
