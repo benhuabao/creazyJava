3b26763ca6e5b616299db0aaee7a5772bcf1dfb2
fengchao0223.ts <fengchao0223@thundersoft.com>
2019-09-16 18:21:35 +0800

    chapter13

---
+++ otto/src/chapter13/excuteDDL.java
@@ -0,0 +1,13 @@
+package chapter13;
+
+import java.util.Properties;
+
+public class excuteDDL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	Properties pros = new Properties();
+	
+}
